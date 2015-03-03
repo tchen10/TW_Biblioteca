501d0e70662557de145ad1db558d022d481dac15
Sebastian Radloff <sradloff@sradloff.local>
2015-03-03 17:00:16 -0600

    created and passed user story for listing all book details, but refactoring

--- src/com/thoughtworks/biblioteca/Book.java
+++ src/com/thoughtworks/biblioteca/Book.java
@@ -11,6 +11,7 @@ public class Book {
     private String author;
     private int yearPublished;
     private PrintStream printStream;
+    private static PrintStream classPrintStream;
     private static ArrayList<Book> books = new ArrayList<Book>();
 
     public Book(String title, String author, int yearPublished, PrintStream printStream) {
@@ -18,6 +19,7 @@ public class Book {
         this.author = author;
         this.yearPublished = yearPublished;
         this.printStream = printStream;
+        classPrintStream = printStream;
         books.add(this);
     }
 
@@ -30,5 +32,18 @@ public class Book {
             book.printTitle();
         }
     }
+
+    public static void printAllBookDetails() {
+        classPrintStream.println("Title | Author | Year Published");
+        for (Book book : books) {
+            book.printAllDetails();
+        }
+    }
+
+    public void printAllDetails() {
+        printStream.println(title + " | " + author + " | " + yearPublished);
+    }
+
+
 
 }
